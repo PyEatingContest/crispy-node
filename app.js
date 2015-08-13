7fa200b0279b5e55302a990fc8ec66046a69afe7
PyEatingContest <PyEatingContest@users.noreply.github.com>
2015-08-13 08:20:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.get('/oauth_callback', function (request, response) {
                 request.session.oauth_access_token = oauth_access_token;
                 request.session.oauth_access_token_secret = oauth_access_token_secret;
                 //var ex_data = request.session.oauth_access_token;
-                var ex_data = JSON.stringify(request);
+                //var ex_data = JSON.stringify(request);
                 fitbitClient.setToken(oauth_access_token);
                 fitbitClient.setTokenSecret(oauth_access_token_secret);
                 
@@ -67,7 +67,7 @@ app.get('/oauth_callback', function (request, response) {
                 var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(result));
                 console.log("Warping to: " + location);
                 response.redirect(location);
-                response.render('index', {ex_data: ex_data}); //
+                //response.render('index', {ex_data: ex_data}); //
             }
     });
    
