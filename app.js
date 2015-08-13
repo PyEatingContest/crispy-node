713328fb5aba3188b2e7e379974464573cf1e440
PyEatingContest <PyEatingContest@users.noreply.github.com>
2015-08-13 11:17:10 -0400

    Render response data test

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ app.get('/oauth_callback', function (request, response) {
                 };
                 var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(result));
                 console.log("Warping to: " + location);
-                response.redirect(location);
-                //response.render('index', {ex_data: ex_data}); //
+                //response.redirect(location);
+                response.render('index', {ex_data: ex_data}); //
             }
     });
    
